01551600ac1004be1b65b61fe0d87273b0bdb772
unknown <samuel.gil-r@labinfo.is.escuelaing.edu.co>
2026-03-24 18:20:46 -0500

    comit 6:20

--- MathService/src/main/java/org/example/FibonacciController.java
+++ MathService/src/main/java/org/example/FibonacciController.java
@@ -1,11 +1,15 @@
 package org.example.MathService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 @RestController
 
 public class FibonacciController {
     @GetMapping("/fibonacci")
-    public List<long> fibonacci(RequestParam int numero){
+    public <numero> List<long> fibonacci(RequestParam int numero){
         List<long> serie = new ArrayList<>();
         long a = 0 , b = 1;
         for (int i = 0; i < numero; i++) {
---
+++ MathService/src/test/java/org/example/MathService/MathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.MathService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
