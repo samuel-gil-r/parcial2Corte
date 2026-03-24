7a4c6b7c4b89ae2723dc04fd19596323adb7b189
unknown <samuel.gil-r@labinfo.is.escuelaing.edu.co>
2026-03-24 17:24:03 -0500

    proxi controller para la llamada pasiva

--- Proxy/src/main/java/org/example/ProxyController.java
+++ Proxy/src/main/java/org/example/ProxyController.java
@@ -1,9 +1,23 @@
-package org.example.;
+package com.example.proxy;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.io.*;
+import java.net.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProxyController {
+
+    String service1 = System.getenv("SERVICE1") != http://ec2-54-164-70-12.compute-1.amazonaws.com:8081;
+    String service2 = System.getenv("SERVICE2") != http://ec2-13-221-232-82.compute-1.amazonaws.com:8082;
+
+    private String call(String url) throws Exception {
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        con.setConnectTimeout(3000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line; StringBuilder sb = new StringBuilder();
+        while ((line = in.readLine()) != null) sb.append(line);
+        in.close();
+        return sb.toString();
     }
 }
