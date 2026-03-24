c29ada75a68f8ffe141a2ecaba398ce49323e4df
unknown <samuel.gil-r@labinfo.is.escuelaing.edu.co>
2026-03-24 17:13:29 -0500

    first commit

---
+++ MathService/src/main/java/org/example/MathApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MathApplication.class, args);
+    }
+}
---
+++ Proxy/src/main/java/org/example/ProxyController.java
@@ -0,0 +1,9 @@
+package org.example.;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProxyController {
+    }
+}
